ec4f7f2343eef1cb294fa0388304cdc51887557f
Chami Rachid <chamirachid1@gmail.com>
2019-09-20 03:36:27 -0700

    Metamask Connection repaired + transactions get sent successfuly

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,42 +1,42 @@
 pragma solidity ^0.5.8;
 
 contract Exchange {
-    
-    // An item
-    struct Item {
-        uint id;
-        string name;
-        string description;
-        uint price;
-        address payable owner;
-        bool sold;
-    }
 
-    // Objects available
-    mapping(uint => Item) public items;
-    
-    // Store Items count
-    uint public itemsCount;
+  // An item
+  struct Item {
+    uint id;
+    string name;
+    string description;
+    uint price;
+    address payable owner;
+    bool sold;
+  }
 
-    constructor () public {
-    	add_Item ("Item1", "Desc1", 0x8FB973565A620DB066B5791c42F84eCe8256e258, 20);
-        itemsCount = 1;
-    }
+  // Objects available
+  mapping(uint => Item) public items;
 
-    function add_Item (string memory _name, string memory _desc, address payable _owner, uint _price) public{
-        // TODO: Add fields verifications
-        // TODO: verify duplicate objects (by name) 
-        Item memory  t_item = Item (itemsCount, _name, _desc, _price, _owner, false);
-        items[itemsCount] = t_item;
-        itemsCount++;
-    }
+  // Store Items count
+  uint public itemsCount;
 
-    function buy_Item (uint _id) payable public {
-        // TODO: Verify object exists
-        require(_id < itemsCount);
-        require(0 <= _id);
-        // TODO: Verify if price is enough to sell
-        items[_id].owner.transfer(items[_id].price * 1000000000000000000); // Not sure of this will send the money...
-        items[_id].sold = true;
-    }
+  constructor () public {
+    add_Item ("Item1", "Desc1", 0x8FB973565A620DB066B5791c42F84eCe8256e258, 20);
+    itemsCount = 1;
+  }
+
+  function add_Item (string memory _name, string memory _desc, address payable _owner, uint _price) public{
+    // TODO: Add fields verifications
+    // TODO: verify duplicate objects (by name)
+    Item memory  t_item = Item (itemsCount, _name, _desc, _price, _owner, false);
+    items[itemsCount] = t_item;
+    itemsCount++;
+  }
+
+  function buy_Item (uint _id) payable public {
+    // TODO: Verify object exists
+    require(_id < itemsCount);
+    require(0 <= _id);
+    // TODO: Verify if price is enough to sell
+    items[_id].owner.transfer(items[_id].price); // Not sure of this will send the money...
+    items[_id].sold = true;
+  }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -10,15 +10,13 @@ App = {
   initWeb3: function() {
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
-      //App.web3Provider = web3.currentProvider;
-      //web3 = new Web3(web3.currentProvider);
-      App.web3Provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
-      web3 = new Web3(App.web3Provider);
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+      window.ethereum.enable();
     } else {
       // Specify default instance if no web3 instance provided
-      // App.web3Provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
-      // web3 = new Web3(App.web3Provider);
-      
+      App.web3Provider = new Web3.providers.HttpProvider('HTTP://127.0.0.1:7545');
+      web3 = new Web3(App.web3Provider);
     }
     return App.initContract(); // Goto initContract function
   },
@@ -34,91 +32,100 @@ App = {
     });
   },
 
-render: function() {
-  var exchangeInstance;
-  var loader = $("#loader");
-  var content = $("#content");
+  render: function() {
+    var exchangeInstance;
+    var loader = $("#loader");
+    var content = $("#content");
 
-  loader.show();
-  content.hide();
+    loader.show();
+    content.hide();
 
-  // Load account data
-  web3.eth.getCoinbase(function(err, account) {
-    if (err === null) {
-      App.account = account;
-      $("#accountAddress").html("Your Account: " + account);
-    }
-  });
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
 
-  // Load contract data
-  App.contracts.Exchange.deployed().then(function(instance) {
-    exchangeInstance = instance;
-    return exchangeInstance.itemsCount();
-  }).then(function(itemsCount) {
-    var itemsResults = $("#itemsResults");
-    itemsResults.empty();
+    // Load contract data
+    App.contracts.Exchange.deployed().then(function(instance) {
+      exchangeInstance = instance;
+      return exchangeInstance.itemsCount();
+    }).then(function(itemsCount) {
+      var itemsResults = $("#itemsResults");
+      itemsResults.empty();
 
-    var candidatesSelect = $('#itemsSelect');
-    candidatesSelect.empty();
+      var candidatesSelect = $('#itemsSelect');
+      candidatesSelect.empty();
 
-    for (var i = 0; i < itemsCount; i++) {
-      exchangeInstance.items(i).then(function(item) {
-        var id = item[0];
-        var name = item[1];
-        var desc = item[2];
-        var price = item[3];
-        var owner = item[4];
-        var available = item[5];
-
-		console.log(available);
-		
-		if (available == true) available = "No";
-		else if (available == false) available = "Yes";
-		
-        // Render items Result
-        var itemTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + desc + "</th><td>" + price + "</th><td>" + owner + "</th><td>" + available + "</td></tr>"
-        itemsResults.append(itemTemplate);
-
-        // Render candidate ballot option
-        var itemOption = "<option value='" + id + "' >" + name + "</ option>"
-        candidatesSelect.append(itemOption);
-      });
-      var pri ='ss'
-      exchangeInstance.items(i).then(function(item) {pri =item[3];});
-      console.log(pri);
-    }
-    loader.hide();
-    content.show();
-  }).catch(function(error) {
-    //console.warn(error);
-  });
+      for (var i = 0; i < itemsCount; i++) {
+        exchangeInstance.items(i).then(function(item) {
+          var id = item[0];
+          var name = item[1];
+          var desc = item[2];
+          var price = item[3];
+          var owner = item[4];
+          var available = item[5];
 
 
-}, 
-buyItem: function() {
+          if (available == true) available = "No";
+          else if (available == false) available = "Yes";
+
+          // Render items Result
+          var itemTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + desc + "</th><td>" + price + "</th><td>" + owner + "</th><td>" + available + "</td></tr>"
+          itemsResults.append(itemTemplate);
+
+          // Render candidate ballot option
+          var itemOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(itemOption);
+        });
+        var pri = 'ss'
+        exchangeInstance.items(i).then(function(item) {
+          pri = item[3];
+        });
+      }
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
+
+
+  },
+  buyItem: function() {
     var itemID = $('#itemsSelect').val();
     App.contracts.Exchange.deployed().then(function(instance) {
-    
-      return instance.buy_Item(itemID, { from: App.account, gas:800000, value: 2 });
+
+      return instance.buy_Item(itemID, {
+        from: App.account,
+        gas: 800000,
+        value: web3.toWei('2', 'ether')
+      });
     }).then(function(result) {
       // Wait for items to update
       $("#content").hide();
       $("#loader").show();
+      console.log(result);
     }).catch(function(err) {
       console.error(err);
     });
-  }, 
-addItem: function() {
-	
-	var name = $('#name').val()
-	var desc = $('#desc').val()
-	var price = $('#price').val()
-	var addr = $('#owner').val()
+  },
+  addItem: function() {
 
-	App.contracts.Exchange.deployed().then(function(instance) {
-      return instance.add_Item(name, desc, addr, price, { from: App.account, gas:800000 });
+    var name = $('#name').val()
+    var desc = $('#desc').val()
+    var price = $('#price').val()
+    var addr = $('#owner').val()
+
+    App.contracts.Exchange.deployed().then(function(instance) {
+      return instance.add_Item(name, desc, addr, price, {
+        from: App.account,
+        gas: 800000
+      });
     }).then(function(result) {
-		$('#success').show();
+      console.log(result);
+      $('#success').show();
     }).catch(function(err) {
       console.error(err);
     });
